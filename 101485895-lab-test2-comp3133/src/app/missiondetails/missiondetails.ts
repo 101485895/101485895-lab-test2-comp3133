f9a200f49b49029e21ebb574211bb0300b2af3a0
101485895 <101485895@georgebrown.ca>
2026-04-08 18:46:25 -0400

    working missiondetails for each mission

--- 101485895-lab-test2-comp3133/src/app/missiondetails/missiondetails.ts
+++ 101485895-lab-test2-comp3133/src/app/missiondetails/missiondetails.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Spacexapi } from '../network/spacexapi';
+import { Mission } from '../models/mission';
 
 @Component({
   selector: 'app-missiondetails',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './missiondetails.html',
   styleUrls: ['./missiondetails.css']
 })
-export class Missiondetails {
+export class Missiondetails implements OnInit {
+  mission?: Mission;
 
+  constructor(
+    private route: ActivatedRoute,
+    private spacexService: Spacexapi,
+    private cdr: ChangeDetectorRef,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit(): void {
+    const flightNumber = this.route.snapshot.paramMap.get('flight_number');
+
+    if (flightNumber) {
+      this.spacexService.getMissionByFlightNumber(flightNumber).subscribe({
+        next: (data) => {
+          this.ngZone.run(() => {
+            this.mission = data;
+            this.cdr.detectChanges();
+          });
+        },
+        error: (err) => {
+          console.error('error loading mission details:', err);
+        }
+      });
+    }
+  }
 }
